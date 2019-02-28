dab1cf00f676393f5f1f4348e71c2bd949b5b734
Валерий <kaktuz34@gmail.com>
2019-02-28 11:19:56 +0200

    finish

--- src/js/components/_headerToggler.js
+++ src/js/components/_headerToggler.js
@@ -1,5 +1,17 @@
-import { OPEN } from '../constants';
+import { OPEN, BODY } from '../constants';
+const close = $('.js-mob-toggler');
+const header = $('.js-header');
+close.on('click', () => {
+  if (!header.hasClass(OPEN)) {
+    header.addClass(OPEN);
+  }
+  else {
+    header.removeClass(OPEN);
+  }
+});
 
-$('.js-mob-toggler').on('click', () => {
-  $('.js-header').toggleClass(OPEN);
+
+BODY.on('click', e => {
+  if ($(e.target).closest(close).length ) return;
+  header.removeClass(OPEN);
 });
