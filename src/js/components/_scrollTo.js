f21039f632bc79a8477fdb72b36e30629ec13978
KopylovVitalik <vitalik.kopylov@gmail.com>
2019-03-01 13:41:25 +0200

    scroll to fixed

--- src/js/components/_scrollTo.js
+++ src/js/components/_scrollTo.js
@@ -1,8 +1,7 @@
-import {WIN, ACTIVE} from '../constants';
-import {SCROLL_TO} from '../utils';
+import { WIN, ACTIVE } from '../constants';
+import { SCROLL_TO } from '../utils';
 
-;(function() {
-
+(function() {
   let $blockItem = $('.js-navigation-section');
   let link = $('.js-menu-link');
 
@@ -12,22 +11,24 @@ import {SCROLL_TO} from '../utils';
       let $that = $(this);
       let id = $that.attr('id');
       let headerHeight = $('.header').innerHeight();
-      if (Math.floor($that.offset().top - headerHeight) <= $scrollPos && Math.floor($that.offset().top - headerHeight) + $that.innerHeight() >= $scrollPos) {
+      if (
+        Math.floor($that.offset().top - headerHeight) <= $scrollPos &&
+        Math.floor($that.offset().top - headerHeight) + $that.innerHeight() >=
+          $scrollPos
+      ) {
         link.removeClass(ACTIVE);
         $('[href="#' + id + '"]').addClass(ACTIVE);
       }
-
     });
-  };
+  }
 
   link.on('click', function(event) {
     event.preventDefault();
     let thisAttr = $(this).attr('href');
     let position = $(thisAttr).offset().top;
     let headerHeight = $('.header').outerHeight();
-    SCROLL_TO(Math.floor(position));
+    SCROLL_TO(Math.floor(position - headerHeight));
   });
 
   WIN.on('scroll touchend', onScroll);
-
 })();
